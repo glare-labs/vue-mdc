446a39e9e88e81de600b333d24c8fbee076dff5f
bre 97 <by.sgatn3@Gmail.com>
2023-11-21 17:47:50 +0800

    feat: add tokens, inclding material colors, duration and curves

---
+++ src/utils/tokens.ts
@@ -0,0 +1,60 @@
+
+export const tokens = {
+    /**
+     * Material Pattle
+     */
+    primary: 'var(--md-sys-color-primary)',
+    onPrimary: 'var(--md-sys-color-on-primary)',
+    primaryContainer: 'var(--md-sys-color-primary-container)',
+    onPrimaryContainer: 'var(--md-sys-color-on-primary-container)',
+    secondary: 'var(--md-sys-color-secondary)',
+    onSecondary: 'var(--md-sys-color-on-secondary)',
+    secondaryContainer: 'var(--md-sys-color-secondary-container)',
+    onSecondaryContainer: 'var(--md-sys-color-on-secondary-container)',
+    tertiary: 'var(--md-sys-color-tertiary)',
+    onTertiary: 'var(--md-sys-color-on-tertiary)',
+    tertiaryContainer: 'var(--md-sys-color-tertiary-container)',
+    onTertiaryContainer: 'var(--md-sys-color-on-tertiary-container)',
+    error: 'var(--md-sys-color-error)',
+    onError: 'var(--md-sys-color-on-error)',
+    errorContainer: 'var(--md-sys-color-error-container)',
+    onErrorContainer: 'var(--md-sys-color-on-error-container)',
+    background: 'var(--md-sys-color-background)',
+    onBackground: 'var(--md-sys-color-on-background)',
+    surface: 'var(--md-sys-color-surface)',
+    onSurface: 'var(--md-sys-color-on-surface)',
+    surfaceVariant: 'var(--md-sys-color-surface-variant)',
+    onSurfaceVariant: 'var(--md-sys-color-on-surface-variant)',
+    outline: 'var(--md-sys-color-outline)',
+    outlineVariant: 'var(--md-sys-color-outline-variant)',
+    shadow: 'var(--md-sys-color-shadow)',
+    scrim: 'var(--md-sys-color-scrim)',
+    inverseSurface: 'var(--md-sys-color-inverse-surface)',
+    inverseOnSurface: 'var(--md-sys-color-inverse-on-surface)',
+    inversePrimary: 'var(--md-sys-color-inverse-primary)',
+
+    /**
+     * Duration
+     */
+    durationUltraFast: '50ms',
+    durationFaster: '100ms',
+    durationFast: '150ms',
+    durationNormal: '200ms',
+    durationGentle: '250ms',
+    durationSlow: '300ms',
+    durationSlower: '400ms',
+    durationUltraSlow: '500ms',
+
+    /**
+     * Curves
+     */
+    curveAccelerateMax: 'cubic-bezier(0.9,0.1,1,0.2)',
+    curveAccelerateMid: 'cubic-bezier(1,0,1,1)',
+    curveAccelerateMin: 'cubic-bezier(0.8,0,0.78,1)',
+    curveDecelerateMax: 'cubic-bezier(0.1,0.9,0.2,1)',
+    curveDecelerateMid: 'cubic-bezier(0,0,0,1)',
+    curveDecelerateMin: 'cubic-bezier(0.33,0,0.1,1)',
+    curveEasyEaseMax: 'cubic-bezier(0.8,0,0.2,1)',
+    curveEasyEase: 'cubic-bezier(0.33,0,0.67,1)',
+    curveLinear: 'cubic-bezier(0,0,1,1)',
+}
