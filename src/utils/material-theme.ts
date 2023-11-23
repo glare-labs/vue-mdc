89acdc354bce4c24d10991a0b0d5aa648e0e1db3
bre 97 <by.sgatn3@Gmail.com>
2023-11-23 13:56:49 +0800

    feat: add tokens, including surfaceContainer, borderRadius, fonts, shadow, spacing

--- src/utils/material-theme.ts
+++ src/utils/material-theme.ts
@@ -12,5 +12,25 @@ export function GenerateMaterialTheme(
 
     // Apply the theme to the body by updating custom properties for material tokens
     applyTheme(theme, { target, dark })
+    MakeSurfaceContainer(target, dark)
+}
 
+/**
+ * @see ONLY DEV IN EMVIRONMENT
+ */
+function MakeSurfaceContainer(target: HTMLElement, dark: boolean) {
+    if(!dark) {
+        target.style.setProperty('--md-sys-color-surface-container-lowest', '#ffffff')
+        target.style.setProperty('--md-sys-color-surface-container-low', '#f1f5ec')
+        target.style.setProperty('--md-sys-color-surface-container', '#ebefe7')
+        target.style.setProperty('--md-sys-color-surface-container-high', '#e5e9e1')
+        target.style.setProperty('--md-sys-color-surface-container-highest', '#e0e4dc')
+    } else {
+        target.style.setProperty('--md-sys-color-surface-container-lowest', '#0b0f0b')
+        target.style.setProperty('--md-sys-color-surface-container-low', '#181d18')
+        target.style.setProperty('--md-sys-color-surface-container', '#1c211c')
+        target.style.setProperty('--md-sys-color-surface-container-high', '#262b26')
+        target.style.setProperty('--md-sys-color-surface-container-highest', '#313630')
+
+    }
 }
--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -32,6 +32,12 @@ export const tokens = {
     inverseSurface: 'var(--md-sys-color-inverse-surface)',
     inverseOnSurface: 'var(--md-sys-color-inverse-on-surface)',
     inversePrimary: 'var(--md-sys-color-inverse-primary)',
+    surfaceContainerLowest: 'var(--md-sys-color-surface-container-lowest)',
+    surfaceContainerLow: 'var(--md-sys-color-surface-container-low)',
+    surfaceContainer: 'var(--md-sys-color-surface-container)',
+    surfaceContainerHigh: 'var(--md-sys-color-surface-container-high)',
+    surfaceContainerHighest: 'var(--md-sys-color-surface-container-highest)',
+
 
     /**
      * Duration
@@ -57,4 +63,106 @@ export const tokens = {
     curveEasyEaseMax: 'cubic-bezier(0.8,0,0.2,1)',
     curveEasyEase: 'cubic-bezier(0.33,0,0.67,1)',
     curveLinear: 'cubic-bezier(0,0,1,1)',
+
+    /**
+     * Border Width
+     */
+    strokeWidthThin: '1px',
+    strokeWidthThick: '2px',
+    strokeWidthThicker: '3px',
+    strokeWidthThickest: '4px',
+
+    /**
+     * Spacing V -
+     */
+    spacingVerticalNone: '0',
+    spacingVerticalXXS: '2px',
+    spacingVerticalXS: '4px',
+    spacingVerticalSNudge: '6px',
+    spacingVerticalS: '8px',
+    spacingVerticalMNudge: '10px',
+    spacingVerticalM: '12px',
+    spacingVerticalL: '16px',
+    spacingVerticalXL: '20px',
+    spacingVerticalXXL: '24px',
+    spacingVerticalXXXL: '32px',
+
+    /**
+     * Spacing H |
+     */
+    spacingHorizontalNone: '0',
+    spacingHorizontalXXS: '2px',
+    spacingHorizontalXS: '4px',
+    spacingHorizontalSNudge: '6px',
+    spacingHorizontalS: '8px',
+    spacingHorizontalMNudge: '10px',
+    spacingHorizontalM: '12px',
+    spacingHorizontalL: '16px',
+    spacingHorizontalXL: '20px',
+    spacingHorizontalXXL: '24px',
+    spacingHorizontalXXXL: '32px',
+
+    /**
+     * Border Radius
+     */
+    borderRadiusNone: '0',
+    borderRadiusSmall: '2px',
+    borderRadiusMedium: '4px',
+    borderRadiusLarge: '6px',
+    borderRadiusXLarge: '8px',
+    borderRadiusCircular: '10000px',
+
+    /**
+     * Font Family
+     */
+    fontFamilyBase: '\'Segoe UI\', \'Segoe UI Web (West European)\', -apple-system, BlinkMacSystemFont, Roboto, \'Helvetica Neue\', sans-serif',
+    fontFamilyMonospace: 'Consolas, \'Courier New\', Courier, monospace',
+    fontFamilyNumeric: 'Bahnschrift, \'Segoe UI\', \'Segoe UI Web (West European)\', -apple-system, BlinkMacSystemFont, Roboto, \'Helvetica Neue\', sans-serif',
+
+    /**
+     * Font Size
+     */
+    fontSizeBase100: '10px',
+    fontSizeBase200: '12px',
+    fontSizeBase300: '14px',
+    fontSizeBase400: '16px',
+    fontSizeBase500: '20px',
+    fontSizeBase600: '24px',
+    fontSizeHero700: '28px',
+    fontSizeHero800: '32px',
+    fontSizeHero900: '40px',
+    fontSizeHero1000: '68px',
+    
+    /**
+     * Font Weight
+     */
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightSemibold: 600,
+    fontWeightBold: 700,
+
+    /**
+     * Font Line Height
+     */
+    lineHeightBase100: '14px',
+    lineHeightBase200: '16px',
+    lineHeightBase300: '20px',
+    lineHeightBase400: '22px',
+    lineHeightBase500: '28px',
+    lineHeightBase600: '32px',
+    lineHeightHero700: '36px',
+    lineHeightHero800: '40px',
+    lineHeightHero900: '52px',
+    lineHeightHero1000: '92px',
+
+    /**
+     * Shadow
+     */
+    shadow2: '0 0 2px color-mix(in srgb, var(--md-sys-color-shadow) 12%, transparent), 0 1px 2px color-mix(in srgb, var(--md-sys-color-shadow) 14%, transparent)',
+    shadow4: '0 0 2px color-mix(in srgb, var(--md-sys-color-shadow) 12%, transparent), 0 2px 4px color-mix(in srgb, var(--md-sys-color-shadow) 14%, transparent)',
+    shadow8: '0 0 2px color-mix(in srgb, var(--md-sys-color-shadow) 12%, transparent), 0 4px 8px color-mix(in srgb, var(--md-sys-color-shadow) 14%, transparent)',
+    shadow16: '0 0 2px color-mix(in srgb, var(--md-sys-color-shadow) 12%, transparent), 0 8px 16px color-mix(in srgb, var(--md-sys-color-shadow) 14%, transparent)',
+    shadow28: '0 0 8px color-mix(in srgb, var(--md-sys-color-shadow) 12%, transparent), 0 14px 28px color-mix(in srgb, var(--md-sys-color-shadow) 14%, transparent)',
+    shadow64: '0 0 8px color-mix(in srgb, var(--md-sys-color-shadow) 12%, transparent), 0 32px 64px color-mix(in srgb, var(--md-sys-color-shadow) 14%, transparent)',
+
 }
