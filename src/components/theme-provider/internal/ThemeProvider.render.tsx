e9274d5b43c4d4c37421c7e86f8c052c4ec86c83
bre 97 <by.sgatn3@Gmail.com>
2023-11-24 19:47:55 +0800

    fix: remove toRef in theme provider

--- src/components/theme-provider/internal/ThemeProvider.render.tsx
+++ src/components/theme-provider/internal/ThemeProvider.render.tsx
@@ -1,24 +1,33 @@
 import { GenerateMaterialTheme } from '@/utils/material-theme'
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent } from 'vue'
 import { props, slots } from './ThemeProvider.type'
 
-export const renderThemeProvider = () => defineComponent({
+export const renderThemeProvider = defineComponent({
     name: 'MAMVThemeProvider',
     props,
     slots,
-    setup(props, { slots }) {
-        const target = ref()
-        
-        onMounted(() => {
-            GenerateMaterialTheme(target.value, props.sourceColor.toString(), ['true'].includes(props.dark.toString()))
-        })
-
-        return () => (
-            <div ref={target}>
+    render() {
+        return (
+            <div>
                 {
-                    slots.default()
+                    this.$slots.default && this.$slots.default()
                 }
             </div>
         )
+    },
+    methods: {
+        generateTheme() {
+            GenerateMaterialTheme(
+                this.$el,
+                this.sourceColor.toString(),
+                ['true'].includes(this.dark.toString())
+            )
+        }
+    },
+    mounted() {
+        this.generateTheme()
+    },
+    updated() {
+        this.generateTheme()
     },
 })
--- src/components/theme-provider/internal/index.ts
+++ src/components/theme-provider/internal/index.ts
@@ -1,5 +1,4 @@
 export * from './ThemeProvider.render'
-import { toRef } from 'vue'
 import { renderThemeProvider } from './ThemeProvider.render'
 
-export const ThemeProvider = toRef(() => renderThemeProvider())
+export const ThemeProvider = renderThemeProvider
