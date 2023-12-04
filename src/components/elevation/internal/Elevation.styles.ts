cb53843fcec0727d997418e102b4b9cc7440bd56
bre 97 <by.sgatn3@Gmail.com>
2023-12-04 22:42:58 +0800

    fix: fix elevation css

--- src/components/elevation/internal/Elevation.styles.ts
+++ src/components/elevation/internal/Elevation.styles.ts
@@ -1,3 +1,4 @@
+import { tokens } from '@/utils/tokens'
 import { StyleSheet } from 'aphrodite'
 
 export const elevationStyles = StyleSheet.create({
@@ -8,6 +9,7 @@ export const elevationStyles = StyleSheet.create({
         transitionDuration: 'inherit',
         transitionProperty: 'inherit',
         transitionTimingFunction: 'inherit',
+        zIndex: -1,
     },
     after: {
         borderRadius: 'inherit',
@@ -16,7 +18,18 @@ export const elevationStyles = StyleSheet.create({
         transitionDuration: 'inherit',
         transitionTimingFunction: 'inherit',
         transitionProperty: 'box-shadow, opacity',
-        opacity: 0.15
+        opacity: 0.15,
+        '--l1-y': 'clamp(0, var(--elevation-level), 1)',
+        '--l2-y': 'clamp(0, var(--elevation-level) - 1, 1)',
+        '--l3to5-y': 'calc(2 * clamp(0, var(--elevation-level) - 2, 3))',
+        '--y': 'calc(1px * (var(--l1-y) + var(--l2-y) + var(--l3to5-y)))',
+        '--l1to2-blur': 'calc(3 * clamp(0, var(--elevation-level), 2))',
+        '--l3to5-blur': 'calc(2 * clamp(0, var(--elevation-level) - 2, 3))',
+        '--blur': 'calc(1px * (var(--l1to2-blur) + var(--l3to5-blur)))',
+        '--l1to4-spread': 'clamp(0, var(--elevation-level), 4)',
+        '--l5-spread': 'calc(2 * clamp(0, var(--elevation-level) - 4, 1))',
+        '--spread': 'calc(1px * (var(--l1to4-spread) + var(--l5-spread)))',
+        boxShadow: `0px var(--y) var(--blur) var(--spread) ${tokens.color.shadow.shadow}`,
     },
     before: {
         borderRadius: 'inherit',
@@ -26,5 +39,14 @@ export const elevationStyles = StyleSheet.create({
         transitionTimingFunction: 'inherit',
         transitionProperty: 'box-shadow, opacity',
         opacity: 0.3,
+        '--l1-y': 'clamp(0, var(--elevation-level), 1)',
+        '--l4-y': 'clamp(0, var(--elevation-level) - 3, 1)',
+        '--l5-y': 'calc(2 * clamp(0, var(--elevation-level) - 4, 1))',
+        '--y': 'calc(1px * (var(--l1-y) + var(--l4-y) + var(--l5-y)))',
+        '--l1-blur': 'calc(2 * clamp(0, var(--elevation-level), 1))',
+        '--l3-blur': 'clamp(0, var(--elevation-level) - 2, 1)',
+        '--l5-blur': 'clamp(0, var(--elevation-level) - 4, 1)',
+        '--blur': 'calc(1px * (var(--l1-blur) + var(--l3-blur) + var(--l5-blur)))',
+        boxShadow: `0px var(--y) var(--blur) 0px ${tokens.color.shadow.shadow}`,
     },
 })
