b140aad4c1e269dc6abdd0f033274928ef70e9df
bre 97 <by.sgatn3@Gmail.com>
2023-12-04 22:43:48 +0800

    chore: add descriptions to icon spec readme

--- src/components/icon/internal/Icon.type.ts
+++ src/components/icon/internal/Icon.type.ts
@@ -1,14 +1,23 @@
 import { PropType, SlotsType } from 'vue'
 
 export const props = {
+
+    /**
+     * @default medium
+     */
     size: {
         default: 'medium',
         type: String as PropType<'small' | 'medium' | 'large'>,
     },
+
+    /**
+     * @default outlined
+     */
     variant: {
         default: 'outlined',
         type: String as PropType<'outlined' | 'rounded' | 'sharp'>
     },
+    
 }
 
 export const slots = Object as SlotsType<{
