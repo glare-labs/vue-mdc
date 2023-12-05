4dde50b1a63b52c35f10fb250a023b0aa8eb25e3
bre 97 <by.sgatn3@Gmail.com>
2023-12-05 12:29:59 +0800

    fix: reset icon's font-size property

--- src/components/icon/internal/Icon.styles.ts
+++ src/components/icon/internal/Icon.styles.ts
@@ -1,6 +1,9 @@
 import { tokens } from '@/utils/tokens'
 import { StyleSheet } from 'aphrodite/no-important'
 
+const innerFontSizeVar = '--inner-icon-font-size'
+const fontSizeVar = '--icon-font-size'
+
 export const iconStyles = StyleSheet.create({
     root: {
         textTransform: 'none',
@@ -16,21 +19,27 @@ export const iconStyles = StyleSheet.create({
     
     small: {
         ...tokens.typescale.bodySmall,
+        [innerFontSizeVar]: '24px',
     },
     medium: {
         ...tokens.typescale.bodyMedium,
+        [innerFontSizeVar]: '24px',
     },
     large: {
         ...tokens.typescale.bodyLarge,
+        [innerFontSizeVar]: '48px',
     },
 
     outlined: {
         fontFamily: 'Material Symbols Outlined',
+        fontSize: `var(${fontSizeVar}, ${innerFontSizeVar})`
     },
     rounded: {
         fontFamily: 'Material Symbols Rounded',
+        fontSize: `var(${fontSizeVar}, ${innerFontSizeVar})`
     },
     sharp: {
         fontFamily: 'Material Symbols Sharp',
+        fontSize: `var(${fontSizeVar}, ${innerFontSizeVar})`
     },
 })
