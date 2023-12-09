d08bbecb785f37f538b24eb03bcef7d48eccb752
bre 97 <by.sgatn3@Gmail.com>
2023-12-09 20:15:44 +0800

    chore: testing elevation

---
+++ src/components/elevation/test/Elevation.test.tsx
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils'
+import { describe, test } from 'vitest'
+import { Elevation } from '../index'
+
+describe('elevation', () => {
+
+    test('elevation: renders', () => {
+        mount(Elevation)
+    })
+
+})
