8851f482ad84ceb5391328eefd9be1e5f40ea6b9
bre 97 <by.sgatn3@Gmail.com>
2023-12-09 20:28:23 +0800

    chore: upload lib export file

---
+++ src/lib/index.ts
@@ -0,0 +1,27 @@
+import { Button } from '@/components/button/index'
+import { Divider } from '@/components/divider'
+import { Elevation } from '@/components/elevation'
+import { Fab, FabExtended } from '@/components/fab'
+import { Icon } from '@/components/icon'
+import { Ripple } from '@/components/ripple'
+import { ThemeProvider } from '@/components/theme-provider/index'
+import { Typography } from '@/components/typography'
+
+export {
+    /**
+     * Those are stable
+     */
+    ThemeProvider,
+    Divider,
+    Elevation,
+    Typography,
+    
+    /**
+     * Those are ready to use
+    */
+    Ripple,
+    Icon,
+    Fab,
+    FabExtended,
+    Button,
+}
