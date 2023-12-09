1e31c5e3281f8aa80269a0e955696b7a85f9391c
bre 97 <by.sgatn3@Gmail.com>
2023-12-09 20:27:05 +0800

    chore: change configs of vite and ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,53 +3,78 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { fileURLToPath, URL } from 'node:url'
-import typescript2 from 'rollup-plugin-typescript2'
 import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        vueJsx(),
-        dts({
-            insertTypesEntry: true,
-        }),
-        typescript2({
-            check: false,
-            include: ['src/components/**/*.vue'],
-            tsconfigOverride: {
-                compilerOptions: {
-                    outDir: 'dist',
-                    sourceMap: true,
-                    declaration: true,
-                    declarationMap: true,
-                },
+export default defineConfig(({ command }) => {
+
+    if (command === 'serve') {
+        return {
+            resolve: {
+                alias: {
+                    '@': fileURLToPath(new URL('./src', import.meta.url))
+                }
             },
-            exclude: ['vite.config.ts']
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            plugins: [
+                vue(),
+                vueJsx(),
+                dts({
+                    insertTypesEntry: true,
+                }),
+            ],
+            root: './catalog',
+            base: '/material-anti-mage-vue',
+            build: {
+                outDir: '../docs',
+                manifest: true,
+                minify: true,
+                cssMinify: true,
+                cssCodeSplit: true,
+                emptyOutDir: true,
+            }
         }
-    },
-    root: './src/docs',
-    build: {
-        outDir: '../../dist',
-        emptyOutDir: true,
-        cssCodeSplit: true,
-        lib: {
-            entry: resolve(__dirname, './src/lib/index.ts'),
-            name: 'material-anti-mage-vue',
-            fileName: 'index',
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue',
+    } else if (command === 'build') {
+        return {
+            resolve: {
+                alias: {
+                    '@': fileURLToPath(new URL('./src', import.meta.url))
+                }
+            },
+            build: {
+                outDir: './dist',
+                emptyOutDir: true,
+                cssCodeSplit: true,
+                sourcemap: true,
+                manifest: true,
+                lib: {
+                    entry: resolve(__dirname, './src/lib/index.ts'),
+                    name: 'material-anti-mage-vue',
+                    fileName: 'index',
+                    formats: ['es'],
+                },
+
+                rollupOptions: {
+                    external: [
+                        'vue',
+                        'material-symbols',
+                        './src/docs/**/*',
+                        './src/components/**/test/*.test.{ts,tsx}'
+                    ],
+                    output: {
+                        globals: {
+                            vue: 'Vue',
+                        },
+                    },
+                    treeshake: true,
                 },
             },
-        },
+            plugins: [
+                vue(),
+                vueJsx(),
+                dts({
+                    insertTypesEntry: true,
+                }),
+            ],
+        }
     }
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,21 @@
+/// <reference types="vitest" />
+
 import { defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
-export default mergeConfig(viteConfig, defineConfig({
-    test: {
-        include: [
-            '../../src/components/**/test/*.test.tsx'
-        ],
-        environment: 'jsdom',
-        coverage: {
-            provider: 'v8',
-            reporter: ['html'],
-            reportsDirectory: '../../test-report/'
-        }
-    },
-}))
+export default defineConfig(e => mergeConfig(
+    viteConfig(e),
+    defineConfig({
+        test: {
+            include: [
+                '../src/components/**/test/*.test.{ts,tsx}'
+            ],
+            environment: 'jsdom',
+            coverage: {
+                provider: 'v8',
+                reporter: ['html'],
+                reportsDirectory: '../test-report/'
+            }
+        },
+    })
+))
