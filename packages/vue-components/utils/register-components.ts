bb36de27a85a8bc726a41044a6818d57ce9b6367
bre 97 <by.sgatn3@Gmail.com>
2023-12-31 13:59:37 +0800

    chore(util): remove unused function

--- packages/vue-components/utils/register-components.ts
+++ packages/vue-components/utils/register-components.ts
@@ -1,37 +1,32 @@
 import { Button, Divider, Elevation, Fab, FabExtended, Icon, IconButton, Ripple, ThemeProvider, Typography } from '../lib'
-import { Component, Plugin } from 'vue'
+import type { Component } from 'vue'
 
-/**
- * Remove 'MAMV' from component's name
- */
-function getComponentName(component: Component) {
-    if (component.name === undefined) throw Error('Unnamed component had been got name fail.')
-    return component.name.slice(4)
-}
 
 /**
  * Register all MAMV Componsnts
- * 
- * the name prefix 'MAMV' has removed
- * 
- * e.g.
- *     MAMVButton -> Button
  * 
  * @example
  * const app = createApp(App)
  * app.use(RegisterAllComponents)
  * app.mount('#root')
  */
-export const RegisterAllComponents = {
+export const RegisterAllComponents = ({
+    // @ts-ignore
     install: (instance) => {
         RegisterComponents(
             ThemeProvider,
+            Button,
+            IconButton,
+            Icon,
             Fab,
             FabExtended,
-            IconButton,
+            Typography,
+            Ripple,
+            Elevation,
+            Divider,
         ).install(instance)
     }
-} as Required<Plugin>
+})
 
 /**
  * Register components that you need
@@ -42,29 +37,10 @@ export const RegisterAllComponents = {
  * app.mount('#root')
  */
 export const RegisterComponents = (...components: Component[]) => ({
+    // @ts-ignore
     install(instance) {
         components
-            .map(e => () => instance.component(getComponentName(e), e))
+            .map(e => () => instance.component(e.name!, e))
             .forEach(e => e())
     },
-}) as Required<Plugin>
-
-/**
- * Register all custom elements.
- * 
- * @example
- * const app = createApp(App)
- * app.use(RegisterAllCustomElements)
- * app.mount('#root')
- */
-export const RegisterAllCustomElements = {
-    install() {
-        customElements.define('am-ripple', Ripple)
-        customElements.define('am-elevation', Elevation)
-        customElements.define('am-typography', Typography)
-        customElements.define('am-button', Button)
-        customElements.define('am-icon', Icon)
-        customElements.define('am-divider', Divider)
-
-    }
-} as Required<Plugin>
+})
