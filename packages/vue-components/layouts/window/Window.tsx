3dd027f2a9a9c94714cd2a4e23b324b4a5ac985b
bre 97 <by.sgatn3@Gmail.com>
2024-01-05 12:29:51 +0800

    feat(layout): layout Window

---
+++ packages/vue-components/layouts/window/Window.tsx
@@ -0,0 +1,26 @@
+import { defineComponent, type SlotsType } from 'vue'
+import css from './Window.module.css'
+
+declare module 'vue' {
+    export interface GlobalComponents {
+        'Am-Layout-Window': typeof renderWindow,
+    }
+}
+
+const renderWindow = defineComponent({
+    name: 'AmLayoutWindow',
+    slots: Object as SlotsType<{
+        default?: void
+    }>,
+    setup(props, { slots }) {
+        return () => (
+            <div class={css.surface}>
+                {slots.default && slots.default()}
+            </div>
+        )
+    },
+})
+
+export {
+    renderWindow as Window
+}
