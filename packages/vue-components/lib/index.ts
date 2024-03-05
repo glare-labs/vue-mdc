8609df3116b25d67bb5f2d7c950887654965b96b
bre 97 <by.sgatn3@Gmail.com>
2024-03-05 18:46:49 +0800

    chore(lib): udate import

--- packages/vue-components/lib/index.ts
+++ packages/vue-components/lib/index.ts
@@ -1,66 +1,53 @@
-import Elevation from '../components/elevation/Elevation.vue'
-import Button from '../components/button/Button.vue'
-import Ripple from '../components/ripple/Ripple.vue'
-import ThemeProvider from '../components/theme-provider/ThemeProvider.vue'
-import Typography from '../components/typography/Typography.vue'
-import Icon from '../components/icon/Icon.vue'
-import Divider from '../components/divider/Divider.vue'
-import IconButton from '../components/icon-button/IconButton.vue'
-import Fab from '../components/fab/Fab.vue'
-import Checkbox from '../components/checkbox/Checkbox.vue'
-import NavigationBar from '../components/navigation-bar/NavigationBar.vue'
-import NavigationTab from '../components/navigation-tab/NavigationTab.vue'
-import Card from '../components/card/Card.vue'
-import ContainerStyleProvider from '../components/container/ContainerStyleProvider.vue'
 
-import Window from '../layouts/window/Window.vue'
+/**
+ * Components
+ */
+export * as Elevation from '../components/elevation/Elevation.vue'
+export * as Button from '../components/button/Button.vue'
+export * as Ripple from '../components/ripple/Ripple.vue'
+export * as ThemeProvider from '../components/theme-provider/ThemeProvider.vue'
+export * as Typography from '../components/typography/Typography.vue'
+export * as Icon from '../components/icon/Icon.vue'
+export * as Divider from '../components/divider/Divider.vue'
+export * as IconButton from '../components/icon-button/IconButton.vue'
+export * as Fab from '../components/fab/Fab.vue'
+export * as Checkbox from '../components/checkbox/Checkbox.vue'
+export * as NavigationBar from '../components/navigation-bar/NavigationBar.vue'
+export * as NavigationTab from '../components/navigation-tab/NavigationTab.vue'
+export * as Card from '../components/card/Card.vue'
+export * as ContainerStyleProvider from '../components/container/ContainerStyleProvider.vue'
+export * as Radio from '../components/radio/Radio.vue'
+export * as Switch from '../components/switch/Switch.vue'
+export * as LinearProgress from '../components/progress/LinearProgress.vue'
+export * as CircularProgress from '../components/progress/CircularProgress.vue'
+export * as Chip from '../components/chip/Chip.vue'
 
-import {
+/**
+ * Layouts
+ */
+export * as Window from '../layouts/window/Window.vue'
+
+/**
+ * Utils
+ */
+export {
+    Tokens,
+    EElevationLevel,
+    EMotionDuration,
+    EMotionEasing,
+    EShape,
+} from '../utils/tokens'
+export {
     RegisterAllComponents,
     RegisterComponents,
     RegisterAllLayouts,
     RegisterLayouts,
 } from '../utils/register-components'
-import { tokens } from '../utils/tokens'
-
 export {
-    /**
-     * Those are stable
-     */
-    Elevation,
-    Button,
-    Ripple,
-    ThemeProvider,
-    Typography,
-    Icon,
-    Divider,
-    IconButton,
-    Fab,
+    MaterialDynamicColorGenerator,
+    MaterialSchemaGenerator,
+    EMaterialContrastLevel,
+    type TMaterialSchemas,
+    type IMaterialGeneratorOptions,
+} from '../utils/material-theme'
 
-    /**
-     * Unstable
-     */
-    Checkbox,
-    NavigationBar,
-    NavigationTab,
-    Card,
-    ContainerStyleProvider,
-
-    /**
-     * Layouts
-     */
-    Window,
-
-    /**
-     * Plugins
-     */
-    RegisterAllComponents,
-    RegisterComponents,
-    RegisterAllLayouts,
-    RegisterLayouts,
-
-    /**
-     * Design system
-     */
-    tokens,
-}
