5d0e5a2b62dd43fcbea7cf9fd723fcb321f5fffc
bre 97 <by.sgatn3@Gmail.com>
2024-05-02 18:14:05 +0800

    refactor: standardized Elevation styles

--- packages/ui/src/components/elevation/Elevation.tsx
+++ packages/ui/src/components/elevation/Elevation.tsx
@@ -11,21 +11,44 @@ export const enum EElevationLevel {
     Level5 = 5,
 }
 
-export const Elevation = defineComponent({
-    name: 'GlareUi-Elevation',
-    props: {
-        level: {
-            type: Number as PropType<TElevationLevel>,
-            default: EElevationLevel.Level0,
-            validator(value: number) {
-                return value >= EElevationLevel.Level0 && value <= EElevationLevel.Level5
-            },
+const props = {
+    /**
+     * 阴影级数, 取值在0到5之间, 包括0和5
+     * 
+     * 请确保**父元素的position为relative**, 并且父元素必须拥有大小
+     * 
+     * + level
+     *   - 0
+     *   - 1
+     *   - 2
+     *   - 3
+     *   - 4
+     *   - 5
+     * 
+     * 可以导入枚举对象EElevationLevel来传入大小
+     * ```html
+     * <div class="relative w-[32px] h-[32px]">
+     *   <Glare.Elevation :level="EElevationLevel.Level3"></Glare.Elevation>
+     * </div>
+     * ```
+     * 
+     * @default 0
+     */
+    level: {
+        type: Number as PropType<TElevationLevel>,
+        default: EElevationLevel.Level0,
+        validator(value: number) {
+            return value >= EElevationLevel.Level0 && value <= EElevationLevel.Level5
         },
     },
-    setup(props, _) {
+}
 
-        return () => (
-            <span class={[css.surface, css[`level-${props.level}`]]}></span>
+export const Elevation = defineComponent({
+    name: 'GlareUi-Elevation',
+    props,
+    render() {
+        return (
+            <span class={[css.elevation, css[`level-${this.level}`]]}></span>
         )
     },
 })
