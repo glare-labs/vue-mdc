7f298ea10105577633ab981c585680f9ccfaf68e
bre 97 <by.sgatn3@Gmail.com>
2024-05-02 20:43:32 +0800

    refactor: standardized Button styles

--- packages/ui/src/components/button/Button.tsx
+++ packages/ui/src/components/button/Button.tsx
@@ -1,7 +1,8 @@
 import { defineComponent, type PropType, type SlotsType } from 'vue'
-import css from './Button.module.css'
 import { Ripple } from '../ripple/Ripple'
 import { Elevation } from '../elevation/Elevation'
+import css from './button.module.css'
+import cssBase from './base.module.css'
 
 export type TButtonAppearance = 'filled' | 'outlined' | 'elevated' | 'filled-tonal' | 'text'
 export type TButtonType = 'button' | 'submit' | 'reset'
@@ -30,53 +31,74 @@ export const enum EButtonShape {
     Square = 'square',
 }
 
+const props = {
+    appearance: {
+        type: String as PropType<TButtonAppearance>,
+        default: EButtonAppearance.Filled,
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+    type: {
+        type: String as PropType<TButtonType>,
+        default: EButtonType.Button,
+    },
+    shape: {
+        type: String as PropType<TButtonShape>,
+        default: EButtonShape.Circular,
+    },
+    iconPosition: {
+        type: String as PropType<TButtonIconPosition>,
+        default: EButtonIconPosition.Left,
+    }
+}
+const slots = {} as SlotsType<{
+    default: void,
+    icon: void
+}>
+
 export const Button = defineComponent({
     name: 'GlareUi-Button',
-    props: {
-        appearance: {
-            type: String as PropType<TButtonAppearance>,
-            default: EButtonAppearance.Filled,
-        },
-        disabled: {
-            type: Boolean,
-            default: false,
-        },
-        type: {
-            type: String as PropType<TButtonType>,
-            default: EButtonType.Button,
-        },
-        shape: {
-            type: String as PropType<TButtonShape>,
-            default: EButtonShape.Circular,
-        },
-    },
-    slots: {} as SlotsType<{
-        default: void,
-        icon: void
-    }>,
+    props,
+    slots,
     render() {
+        const renderIcon = this.$slots.icon && (
+            <span class={cssBase.icon}>
+                {this.$slots.icon()}
+            </span>
+        )
+        const renderLabel = this.$slots.default && (
+            <span class={[cssBase.label]}>
+                {this.$slots.default()}
+            </span>
+        )
+        const renderButton = (
+            <button
+                class={[cssBase.button, this.iconPosition === EButtonIconPosition.Right && cssBase.revert]}
+                type={this.type}
+                disabled={this.disabled}
+                aria-disabled={this.disabled}
+            >
+                {renderIcon}
+                {renderLabel}
+            </button>
+        )
+
         return (
             <div
-                class={[css.surface, css[this.appearance], this.disabled && css.disabled]}
+                class={[css[this.appearance], cssBase.container, this.disabled && cssBase.disabled]}
                 aria-disabled={this.disabled}
+                role='button'
+
             >
                 <Ripple></Ripple>
                 <Elevation></Elevation>
 
-                <div class={css.background}></div>
-                <div class={css.outline}></div>
+                <div aria-hidden="true" class={[cssBase.background]}></div>
+                <div aria-hidden="true" class={[cssBase.outline]}></div>
 
-                <button
-                    class={css.button}
-                    type={this.type}
-                    disabled={this.disabled}
-                    aria-disabled={this.disabled}
-                >
-                    {this.$slots.icon && this.$slots.icon}
-                    <span class={css.label}>
-                        {this.$slots.default && this.$slots.default()}
-                    </span>
-                </button>
+                {renderButton}
             </div>
         )
     }
