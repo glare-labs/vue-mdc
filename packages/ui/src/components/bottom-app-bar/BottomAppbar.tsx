68892002ece356dd900af5c54a78ab5caec11941
Bre 97 <by.sgatn3@Gmail.com>
2024-06-01 15:16:40 +0800

    feat: component BottomAppbar

---
+++ packages/ui/src/components/bottom-app-bar/BottomAppbar.tsx
@@ -0,0 +1,48 @@
+import { defineComponent } from 'vue'
+import { BottomAppbarBase } from './base'
+import css from './bottomAppbar.module.css'
+import cssBase from './base.module.css'
+import { Elevation } from '../elevation/Elevation'
+
+class BottomAppbarComponent {
+    private static readonly props = BottomAppbarBase.propsShared
+    private static readonly slots = BottomAppbarBase.slotsShared
+    private static readonly emits = []
+
+    public static readonly component = defineComponent({
+        name: 'GlareUi-BottomAppbar',
+        props: this.props,
+        slots: this.slots,
+        emits: this.emits,
+        setup(props, ctx) {
+            
+        },
+        render() {
+            const renderBackground = <div aria-hidden="true" class={cssBase.background}></div>
+            const renderDefault = (
+                <ul class={cssBase['button-group']}>
+                    {this.$slots.default && this.$slots.default()}
+                </ul>
+            )
+            const renderActionButton = (
+                <span class={[cssBase['action-button']]}>
+                    <span class={cssBase['action-button-wrapper']}>
+                        {this.$slots['action-button'] && this.$slots['action-button']()}
+                    </span>
+                </span>
+            )
+
+            return (
+                <div class={[css['default-impl'], cssBase.container, this.enableElevation && cssBase['enable-elevation']]}>
+                    {renderBackground}
+                    {renderDefault}
+                    {renderActionButton}
+
+                    <Elevation></Elevation>
+                </div>
+            )
+        }
+    })
+}
+
+export const BottomAppbar = BottomAppbarComponent.component
---
+++ packages/ui/src/components/bottom-app-bar/base.ts
@@ -0,0 +1,25 @@
+import { PropType, SlotsType } from 'vue'
+
+export type TBottomAppbarActionButtonShape = 'square' | 'rounded'
+
+export class BottomAppbarBase {
+    public static readonly propsShared = {
+        actionButtonShape: {
+            type: String as PropType<TBottomAppbarActionButtonShape>,
+            default: 'rounded'
+        },
+
+        enableElevation: {
+            type: Boolean as PropType<boolean>,
+            default: false
+        },
+
+
+
+    }
+
+    public static readonly slotsShared = {} as SlotsType<{
+        default: void
+        'action-button': void
+    }>
+}
