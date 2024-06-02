1d6ba1dcc399202b7ae30316ca6607deeaad33d0
Bre 97 <by.sgatn3@Gmail.com>
2024-06-02 17:07:44 +0800

    refactor: remove material-typescale and provide a CSS implementation based on it

--- packages/ui/src/components/glare-provider/GlareProvider.tsx
+++ packages/ui/src/components/glare-provider/GlareProvider.tsx
@@ -1,16 +1,11 @@
-import { ComputedRef, PropType, SlotsType, computed, defineComponent, provide, ref, watch } from 'vue'
-import { EMaterialContrastLevel, EMaterialVariant, MaterialThemeGenerator, TColor, TMaterialColors, type TMaterialContrastLevel, type TMaterialVariant } from '../../utils/material-theme'
-import { MaterialTypescale } from '../../utils/material-typescale'
+import { PropType, SlotsType, computed, defineComponent, ref, watch } from 'vue'
+import { EMaterialContrastLevel, EMaterialVariant, MaterialThemeGenerator, TColor, type TMaterialContrastLevel, type TMaterialVariant } from '../../utils/material-theme'
+import typoCss from '../typography/MaterialTypographyImplement.module.css'
 
 export const GlareContextInjection = Symbol()
 
-type TPublicData = {
-    themeConfiguration: TThemeConfiguration,
-    tokensObject: TMaterialColors,
-    tokensStyleText: string
-}
 type TSlots = {
-    default: TPublicData
+    default: void
 }
 type TThemeConfiguration = {
     sourceColor: TColor
@@ -19,83 +14,73 @@ type TThemeConfiguration = {
     variant: TMaterialVariant
 
 }
-const Props = {
-    sourceColor: {
-        default: '#123456',
-        type: String as PropType<TColor>
-    },
-    dark: {
-        default: false,
-        type: Boolean as PropType<boolean>
-    },
-    contrastLevel: {
-        default: EMaterialContrastLevel.Default,
-        type: Number as PropType<TMaterialContrastLevel>
-    },
-    variant: {
-        default: EMaterialVariant.TONAL_SPOT,
-        type: Number as PropType<TMaterialVariant>
-    },
-}
 
-export const GlareProvider = defineComponent({
-    name: 'GlareUi-GlareProvider',
-    props: Props,
-    slots: {} as SlotsType<TSlots>,
-    setup(props) {
-        const themeConfiguration = ref<TThemeConfiguration>({
-            sourceColor: props.sourceColor,
-            dark: props.dark,
-            contrastLevel: props.contrastLevel,
-            variant: props.variant,
-        })
-        const setThemeConfiguration = (optional?: Partial<TThemeConfiguration>) => {
-            themeConfiguration.value = {
-                ...themeConfiguration.value,
-                ...optional
-            }
-        }
+class GlareProviderComponent {
+    private static readonly name = 'GlareUi-GlareProvider'
+    private static readonly props = {
+        sourceColor: {
+            default: '#123456',
+            type: String as PropType<TColor>
+        },
+        dark: {
+            default: false,
+            type: Boolean as PropType<boolean>
+        },
+        contrastLevel: {
+            default: EMaterialContrastLevel.Default,
+            type: Number as PropType<TMaterialContrastLevel>
+        },
+        variant: {
+            default: EMaterialVariant.TONAL_SPOT,
+            type: Number as PropType<TMaterialVariant>
+        },
+    }
+    private static readonly slots = {} as SlotsType<TSlots>
 
-        watch(() => props, () => {
-            setThemeConfiguration({
-                contrastLevel: props.contrastLevel,
-                dark: props.dark,
+    public static readonly component = defineComponent({
+        name: this.name,
+        props: this.props,
+        slots: this.slots,
+        setup(props, { slots }) {
+            const root = ref<null | HTMLElement>(null)
+
+            /**
+             * 用于生成颜色的主题配置
+             */
+            const themeConfiguration = ref<TThemeConfiguration>({
                 sourceColor: props.sourceColor,
+                dark: props.dark,
+                contrastLevel: props.contrastLevel,
                 variant: props.variant,
             })
-        })
+            const setThemeConfiguration = (optional?: Partial<TThemeConfiguration>) => {
+                themeConfiguration.value = {
+                    ...themeConfiguration.value,
+                    ...optional
+                }
+            }
+            watch(() => props, () => {
+                setThemeConfiguration({
+                    contrastLevel: props.contrastLevel,
+                    dark: props.dark,
+                    sourceColor: props.sourceColor,
+                    variant: props.variant,
+                })
+            })
+            const tokensStyleText = computed<string>(() => `:root {${MaterialThemeGenerator.toStyleText(MaterialThemeGenerator.generate(props.sourceColor, {
+                contrastLevel: props.contrastLevel,
+                isDark: props.dark,
+                variant: props.variant
+            }))}}}`)
+    
+            return () => (
+                <div ref={root} class={typoCss.typography}>
+                    <style>{ tokensStyleText.value }</style>
+                    {slots.default && slots.default()}
+                </div>
+            )
+        },
+    })
+}
 
-        const tokensObject = computed<TMaterialColors>(() => MaterialThemeGenerator.generate(props.sourceColor, {
-            contrastLevel: props.contrastLevel,
-            isDark: props.dark,
-            variant: props.variant
-        }))
-        const tokensStyleText = computed<string>(() => MaterialThemeGenerator.toStyleText(tokensObject.value))
-
-        const publicData = computed<TPublicData>(() => ({
-            themeConfiguration: themeConfiguration.value,
-            tokensObject: tokensObject.value,
-            tokensStyleText: tokensStyleText.value,
-        }))
-
-        provide<ComputedRef<TPublicData>>(GlareContextInjection, publicData)
-        return {
-            publicData
-        }
-    },
-    computed: {
-        styleTextContent() {
-            return `:root {${this.publicData.tokensStyleText}} ${MaterialTypescale.generate()}`
-        }
-    },
-    render() {
-        return (
-            <>
-                <style type='text/css'>
-                    {this.styleTextContent}
-                </style>
-                {this.$slots.default(this.publicData)}
-            </>
-        )
-    }
-})
+export const GlareProvider = GlareProviderComponent.component
