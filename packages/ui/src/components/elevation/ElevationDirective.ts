67c0bf8916c789aa666297ae0a87f07f8f6a5c53
Bre 97 <by.sgatn3@Gmail.com>
2024-06-03 13:34:29 +0800

    fix: add more parameter detection for v-elevation

--- packages/ui/src/components/elevation/ElevationDirective.ts
+++ packages/ui/src/components/elevation/ElevationDirective.ts
@@ -1,47 +1,88 @@
-import { Directive } from 'vue'
+import { Directive, DirectiveBinding } from 'vue'
 import css from './Elevation.module.css'
+import { TElevationLevel } from './ElevationLevel'
 
 class ElevationDirective {
 
-    public static readonly directive: Directive = {
-        mounted(el: HTMLElement, binding) {
-            const div = document.createElement('span')
-            div.setAttribute('data-standalone', 'true')
-            div.setAttribute('aria-hidden', 'true')
-            div.classList.add(css.elevation)
-            el.appendChild(div)
-            if (typeof binding.value === 'undefined') {
-                console.warn(`It is recommended to specify a parameter for v-elevation, and the recommended parameters range from 0 to 5. If you do not specify a parameter, the default value is 3.`)
-                div.style.setProperty('--glare-ui-elevation-level', `1`)
-            }
-            else if (binding.value >= 0 && binding.value <= 5) {
-                div.classList.add(css[`level-${binding.value}`])
-            } else {
-                console.warn(`It is recommended to set the value of the level setting of the component of Elevation to 0 to 5.`)
-                div.style.setProperty('--glare-ui-elevation-level', binding.value || 1)
-            }
+    private static readonly wrapElevationLevel = (binding: DirectiveBinding<TElevationLevel>) => {
+        /**
+         * When no parameters are provided for v-elevation
+         * @value 1
+         * @example
+         * ```html
+         * <div v-elevation></div>
+         * ```
+         */
+        if (typeof binding.value === 'undefined') {
+            console.warn(`It is recommended to specify a parameter for v-elevation, and the recommended parameters range from 0 to 5. If you do not specify a parameter, the default value is 3.`)
+            return `1`
+        }
+        /**
+         * When providing the correct parameters to v-elevation
+         * @example
+         * ```html
+         * <div v-elevation="3"></div>
+         * ```
+         */
+        else if (binding.value >= 0 && binding.value <= 5) {
+            return `${binding.value}`
+        }
+        /**
+         * When wrong parameters are provided to v-elevation
+         * @value binding.value < 0 ? 0 : 5
+         * @example
+         * ```html
+         * <div v-elevation="999"></div>
+         * ```
+         */
+        else if (binding.value < 0 || binding.value > 5) {
+            console.warn(`It is recommended to set the value of the level setting of the component of Elevation to 0 to 5. If an incorrect argument is provided, the default value is \`${binding.value < 0 ? 0 : 5}\``)
+            return `${binding.value < 0 ? 0 : 5}`
+        }
+        /**
+         * Other cases, such as type errors
+         * @value 0
+         * @example
+         * ```html
+         * <div v-elevation="abc"></div>
+         * ```
+         */
+        else {
+            console.warn(`Incorrect v-elevation data provided \`${binding.value}\`. To safely set elevation, if an incorrect type argument is provided, the default value is 0.`)
+            return `0`
+        }
+    }
+    private static readonly queryElevationElementFromEl = (el: HTMLElement) => {
+        return el.querySelector(`span.${css.elevation}[data-standalone="true"]`) as HTMLElement
+    }
+    private static readonly createElevationElement = () => {
+        const elevationElement = document.createElement('span')
+        elevationElement.setAttribute('data-standalone', 'true')
+        elevationElement.setAttribute('aria-hidden', 'true')
+        elevationElement.classList.add(css.elevation)
+        return elevationElement
+    }
+
+    public static readonly directive: Directive<HTMLElement, TElevationLevel> = {
+        beforeMount: (el) => {
+            el.appendChild(this.createElevationElement())
         },
-        updated(el: HTMLElement, binding) {
-            let targetFound = el.querySelector(`.${css.elevation}[data-standalone="true"]`) as HTMLElement
+        mounted: (el, binding) => {
+            const queriedElevationElement = this.queryElevationElementFromEl(el)
+            const elevationElement = typeof queriedElevationElement !== 'undefined' && queriedElevationElement !== null ? queriedElevationElement : this.createElevationElement()
 
-            if (!targetFound) {
-                targetFound = document.createElement('span')
-                targetFound.setAttribute('aria-hidden', 'true')
-            }
+            const level = this.wrapElevationLevel(binding)
+            elevationElement.style.setProperty('--glare-ui-elevation-level', level)
 
-            targetFound.className = `${css.elevation}`
-            targetFound.style.removeProperty('--glare-ui-elevation-level')
+        },
+        updated: (el, binding) => {
+            const queriedElevationElement = this.queryElevationElementFromEl(el)
+            const elevationElement = typeof queriedElevationElement !== 'undefined' && queriedElevationElement !== null ? queriedElevationElement : this.createElevationElement()
 
-            if (typeof binding.value === 'undefined') {
-                console.warn(`It is recommended to specify a parameter for v-elevation, and the recommended parameters range from 0 to 5. If you do not specify a parameter, the default value is 3`)
-                targetFound.style.setProperty('--glare-ui-elevation-level', `1`)
-            }
-            else if (binding.value >= 0 && binding.value <= 5) {
-                targetFound.classList.add(css[`level-${binding.value}`])
-            } else {
-                console.warn(`It is recommended to set the value of the level setting of the component of Elevation to 0 to 5.`)
-                targetFound.style.setProperty('--glare-ui-elevation-level', binding.value || 1)
-            }
+
+            const level = this.wrapElevationLevel(binding)
+            elevationElement.style.removeProperty('--glare-ui-elevation-level')
+            elevationElement.style.setProperty('--glare-ui-elevation-level', level)
         }
     }
 }
