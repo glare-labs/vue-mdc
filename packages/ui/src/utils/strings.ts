447d2a821c837bc66bd7c9558052846949861e21
Bre 97 <by.sgatn3@Gmail.com>
2024-06-05 20:44:14 +0800

    refactor: string functions are now classified as Strings objects

--- packages/ui/src/utils/strings.ts
+++ packages/ui/src/utils/strings.ts
@@ -1,12 +1,15 @@
-/**
- * 
- * @example
- *   MAMVButton -> m-a-m-v-button
- */
-export function toKebabCase(str: string) {
-    return str.split('').map((letter, idx) => {
-        return letter.toUpperCase() === letter
-            ? `${idx !== 0 ? '-' : ''}${letter.toLowerCase()}`
-            : letter
-    }).join('')
+
+export class Strings {
+    public static isHexColor(hexColor: string) {
+        return /^(#?)([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/.test(hexColor)
+    }
+
+    public static toKebabCase(str: string) {
+        return str.split('').map((letter, idx) => {
+            return letter.toUpperCase() === letter
+                ? `${idx !== 0 ? '-' : ''}${letter.toLowerCase()}`
+                : letter
+        }).join('')
+    }
+
 }
