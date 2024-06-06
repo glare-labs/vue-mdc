c9143bd46f3e418030d4b5723bf4e5c21dc639c8
Bre 97 <by.sgatn3@Gmail.com>
2024-06-06 12:15:14 +0800

    chore: update configurations

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -6,16 +6,13 @@ import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), dts({
-    compilerOptions: {
-      baseUrl: './src',
-    },
-    tsconfigPath: 'tsconfig.json'
+    tsconfigPath: './tsconfig.json'
   })],
   mode: 'lib',
   build: {
     target: 'modules',
     minify: false,
-    ssr: true,
+    ssr: false,
     outDir: './build',
     emptyOutDir: true,
     reportCompressedSize: false,
