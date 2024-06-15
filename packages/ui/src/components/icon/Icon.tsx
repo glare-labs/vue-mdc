f086a4bdfb984ec8afd27b3e5ddaeb8b9b060147
Bre 97 <by.sgatn3@Gmail.com>
2024-06-15 21:53:09 +0800

    refactor: sass icon

--- packages/ui/src/components/icon/Icon.tsx
+++ packages/ui/src/components/icon/Icon.tsx
@@ -1,84 +1,53 @@
 import { defineComponent, type PropType, type SlotsType } from 'vue'
-import css from './icon.module.css'
+import css from './styles/icon.module.scss'
+import { EIconVariant, type TIconVariant } from './IconVariant'
 
-export type TIconSize = 'small' | 'medium' | 'semi-large' | 'large'
-export type TIconVariant = 'outlined' | 'rounded' | 'sharp'
-
-export const enum EIconSize {
-    Small = 'small',
-    Medium = 'medium',
-    SemiLarge = 'semi-large',
-    Large = 'large',
-}
-export const enum EIconVariant {
-    Outlined = 'outlined',
-    Rounded = 'rounded',
-    Sharp = 'sharp',
-}
-
-const props = {
-    /**
-     * 图标大小
-     * 
-     * + size
-     *   - small
-     *   - medium
-     *   - semi-large
-     *   - large
-     * 
-     * 可以导入枚举对象EIconSize来传入大小
-     * ```html
-     * <Icon :size="EIconSize.Large">send</Icon>
-     * <Icon :size="EIconSize.Medium">send</Icon>
-     * <Icon :size="EIconSize.SemiLarge">send</Icon>
-     * <Icon :size="EIconSize.Small">send</Icon>
-     * ```
-     * 
-     * @default small
-     */
-    size: {
-        type: String as PropType<TIconSize>,
-        default: EIconSize.Medium,
-    },
-
-    /**
-     * 图标变体
-     * 
-     * _此选项仅在项目安装了依赖项`material-symbols`时有效_
-     * 
-     * + variant
-     *   - sharp
-     *   - rounded
-     *   - outlined
-     * 
-     * sharp更锐利, rounded更平滑, outlined较为一般化
-     * 
-     * 可以导入枚举对象EIconSize来传入大小
-     * ```html
-     * <Icon :variant="EIconVariant.Sharp">send</Icon>
-     * ```
-     * 
-     * @default rounded
-     */
-    variant: {
-        type: String as PropType<TIconVariant>,
-        default: EIconVariant.Rounded,
-    },
-}
-const slots = {} as SlotsType<{
-    default?: void
-}>
-
-export const Icon = defineComponent({
-    name: 'GlareUi-Icon',
-    props,
-    slots,
-    render() {
-        return (
-            <span class={[css.icon, css[this.size], css[this.variant]]}>
-                {this.$slots.default && this.$slots.default()}
-            </span>
-
-        )
+class IconComponent {
+    private name = `GlareUi-Icon`
+    private props = { 
+        /**
+         * 图标变体
+         * 
+         * _此选项仅在项目安装了依赖项`material-symbols`时有效_
+         * 
+         * + variant
+         *   - sharp
+         *   - rounded
+         *   - outlined
+         * 
+         * sharp更锐利, rounded更平滑, outlined较为一般化
+         * 
+         * 可以导入枚举对象EIconSize来传入大小
+         * ```html
+         * <Icon :variant="EIconVariant.Sharp">send</Icon>
+         * ```
+         * 
+         * @default rounded
+         */
+        variant: {
+            type: String as PropType<TIconVariant>,
+            default: EIconVariant.Rounded,
+        },
     }
-})
+    private slots = {} as SlotsType<{
+        default?: void
+    }>
+
+    
+    public comonent = defineComponent({
+        name: this.name,
+        props: this.props,
+        slots: this.slots,
+        render() {
+            return (
+                <span data-component="icon" class={[css.icon, css[this.variant]]}>
+                    {this.$slots.default && this.$slots.default()}
+                </span>
+    
+            )
+        }
+    })
+    
+}
+
+export const Icon = new IconComponent().comonent
---
+++ packages/ui/src/components/icon/IconVariant.ts
@@ -0,0 +1,7 @@
+export type TIconVariant = 'outlined' | 'rounded' | 'sharp'
+
+export enum EIconVariant {
+    Outlined = 'outlined',
+    Rounded = 'rounded',
+    Sharp = 'sharp',
+}
