189236cc06b7732feaafa9fc90bb7d261c88d083
Bre 97 <by.sgatn3@Gmail.com>
2024-06-17 18:38:40 +0800

    refactor: sass divider

---
+++ packages/ui/src/components/divider/DividerVariant.ts
@@ -0,0 +1,8 @@
+export type TDividerVariant = 'inset-left' | 'middle-inset' | 'inset-right' | 'no-inset'
+
+export const enum EDividerVariant {
+    InsetStart = 'inset-start',
+    MiddleInset = 'middle-inset',
+    InsetEnd = 'inset-end',
+    NoInset = 'no-inset',
+}
