91d4e1438741c7f06ad36a5643c869ec8f3a2434
Bre 97 <by.sgatn3@Gmail.com>
2024-06-19 17:16:38 +0800

    refactor: sass navigation-bar and navigation-tab

--- packages/ui/src/components/navigation-bar/NavigationBar.tsx
+++ packages/ui/src/components/navigation-bar/NavigationBar.tsx
@@ -1,6 +1,6 @@
-import { defineComponent, type ComputedRef, type PropType, type SlotsType } from 'vue'
-import css from './NavigationBar.module.css'
-import tabCss from '../navigation-tab/Navigation.module.css'
+import { defineComponent, type PropType, type SlotsType } from 'vue'
+import tabCss from '../navigation-tab/styles/navigation-tab.module.scss'
+import css from './styles/navigation-bar.module.scss'
 
 export interface INavigationBarProvider {
     currentActiveIndex: number | null
@@ -15,16 +15,9 @@ export const enum ENavigationBarPosition {
     Right = 'right',
 }
 
-export const NavigationBar = defineComponent({
-    name: 'GlareUi-NavigationBar',
-    provide() {
-        return ({
-            [SNavigationBarProvider]: {
-                currentActiveIndex: this.activeIndex,
-            }
-        })
-    },
-    props: {
+class NavigationBarComponent {
+    private name = `GlareUi-NavigationBar`
+    private props = {
         defaultActiveIndex: {
             type: Number,
             default: null,
@@ -37,55 +30,73 @@ export const NavigationBar = defineComponent({
             type: String as PropType<TNavigationBarPosition>,
             default: ENavigationBarPosition.Center,
         },
-    },
-    watch: {
-        activeIndex(val) {
-            this.$emit('update:modelValue', val)
-        },
-    },
-    data(vm) {
-        return ({
-            activeIndex: null as null | number,
-            activeElement: null as HTMLElement | null,
-        })
-    },
-    created() {
-        if (this.defaultActiveIndex !== null) this.$emit('update:modelValue', this.defaultActiveIndex)
-        if (this.defaultActiveIndex !== null && this.modelValue !== null) console.warn('When defaultActiveIndex and v-model are provided at the same time, the value of v-model will be replaced by defaultActiveIndex.')
-    },
-    mounted() {
-        /**
-         * Get all the children that masked 'data-is-tab', count the children and set id and click event to it.
-         * 
-         */
-        const el = (this.$el) as HTMLElement
-        const children = [...el.children].filter(e => e.getAttribute('data-is-tab') !== null) as Array<HTMLElement>
-
-        children.map((e, i) => {
-            e.setAttribute('data-tab-id', i.toString())
-            e.addEventListener('click', (e: Event) => {
-                const element = e.currentTarget as HTMLElement
-                const elementId = element.getAttribute('data-tab-id')
-                if (elementId === null) throw new Error('NO ID FOUND')
-                this.activeIndex = parseInt(elementId)
-                this.activeElement?.classList.remove(tabCss.active)
-                this.activeElement = element
-                this.activeElement.classList.add(tabCss.active)
-            }, true)
-        })
-    },
-    slots: {} as SlotsType<{
-        default: void
-    }>,
-    emits: ['update:modelValue'],
-    render() {
-        return (
-            <div
-                class={[css.surface, css[this.position]]}
-                ref="barRef"
-            >
-                {this.$slots.default && this.$slots.default()}
-            </div>
-        )
     }
-})
+    private slots = {} as SlotsType<{
+        default: void
+    }>
+    private emits = ['update:modelValue']
+
+    public component = defineComponent({
+        name: this.name,
+        props: this.props,
+        slots: this.slots,
+        emits: this.emits,
+        provide() {
+            return ({
+                [SNavigationBarProvider]: {
+                    currentActiveIndex: this.activeIndex,
+                }
+            })
+        },
+        watch: {
+            activeIndex(val) {
+                this.$emit('update:modelValue', val)
+            },
+        },
+        data(vm) {
+            return ({
+                activeIndex: null as null | number,
+                activeElement: null as HTMLElement | null,
+            })
+        },
+        created() {
+            if (this.defaultActiveIndex !== null) this.$emit('update:modelValue', this.defaultActiveIndex)
+            if (this.defaultActiveIndex !== null && this.modelValue !== null) console.warn('When defaultActiveIndex and v-model are provided at the same time, the value of v-model will be replaced by defaultActiveIndex.')
+        },
+        mounted() {
+            /**
+             * Get all the children that masked 'data-is-tab', count the children and set id and click event to it.
+             * 
+             */
+            const el = (this.$el) as HTMLElement
+            const children = [...el.children].filter(e => e.getAttribute('data-is-tab') !== null) as Array<HTMLElement>
+    
+            children.map((e, i) => {
+                e.setAttribute('data-tab-id', i.toString())
+                e.addEventListener('click', (e: Event) => {
+                    const element = e.currentTarget as HTMLElement
+                    const elementId = element.getAttribute('data-tab-id')
+                    if (elementId === null) throw new Error('NO ID FOUND')
+                    this.activeIndex = parseInt(elementId)
+                    this.activeElement?.classList.remove(tabCss.active)
+                    this.activeElement = element
+                    this.activeElement.classList.add(tabCss.active)
+                }, true)
+            })
+        },
+
+        render() {
+            return (
+                <div
+                    data-component="navigation-bar"
+                    class={[css['navigation-bar']]}
+                    ref="barRef"
+                >
+                    {this.$slots.default && this.$slots.default()}
+                </div>
+            )
+        }
+    })
+}
+
+export const NavigationBar = new NavigationBarComponent().component
--- packages/ui/src/components/navigation-tab/NavigationTab.tsx
+++ packages/ui/src/components/navigation-tab/NavigationTab.tsx
@@ -1,50 +1,70 @@
-import { SlotsType, computed, defineComponent, inject } from 'vue'
+import { type PropType, type SlotsType, defineComponent, inject } from 'vue'
 import { Ripple } from '../ripple/Ripple'
-import { INavigationBarProvider, SNavigationBarProvider } from '../navigation-bar/NavigationBar'
-import css from './Navigation.module.css'
+import { type INavigationBarProvider, SNavigationBarProvider } from '../navigation-bar/NavigationBar'
+import css from './styles/navigation-tab.module.scss'
 
-export const NavigationTab = defineComponent({
-    inject: {
+class NavigationTabComponent {
+    private name = `GlareUi-NavigationTab`
+    private props = {
+        label: {
+            type: String as PropType<string>,
+            default: 'Unnamed Tab'
+        }
+    }
+    private slots = {} as SlotsType<{
+        default: void
+        'active-icon': void
+        'inactive-icon': void
+    }>
+    private inject = {
         bar: {
             from: SNavigationBarProvider
         }
-    },
-    setup(props, ctx) {
-        const injection = inject<INavigationBarProvider>(SNavigationBarProvider)
-
-        return {
-            injection
-        }
-    },
-    slots: {} as SlotsType<{
-        default: void,
-        icon: void
-    }>,
-    render() {
-        return (
-            <div
-                data-is-tab
-                class={[css.surface]}
-            >
-                <span class={[css.indicator]}>
-                    {
-                        this.$slots.icon ? this.$slots.icon() :
-                            <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 6C15.31 6 18 8.69 18 12C18 15.31 15.31 18 12 18C8.69 18 6 15.31 6 12C6 8.69 8.69 6 12 6ZM12 4C7.58 4 4 7.58 4 12C4 16.42 7.58 20 12 20C16.42 20 20 16.42 20 12C20 7.58 16.42 4 12 4Z" />
-                            </svg>
-
-                    }
-                </span>
-
-                <span class={[css.label]}>
-                    {
-                        this.$slots.default && this.$slots.default()
-                    }
-                </span>
-
-                <Ripple></Ripple>
-
-                <div data-touch-target class={css['touch-target']}></div>
-            </div >)
     }
-})
+
+    public component = defineComponent({
+        name: this.name,
+        props: this.props,
+        slots: this.slots,
+        inject: this.inject,
+        setup(props, ctx) {
+            const injection = inject<INavigationBarProvider>(SNavigationBarProvider)
+    
+            return {
+                injection
+            }
+        },
+        render() {
+            return (
+                <button
+                    data-component="navigation-tab"
+                    data-is-tab
+                    class={[css['navigation-tab']]}
+                    role='tab'
+                >
+                    <Ripple></Ripple>
+    
+                    <span aria-hidden="true" class={css["icon-content"]}>
+                        <span class={css["active-indicator"]}></span>
+                        <span class={css["icon"]}>
+                            {this.$slots['inactive-icon'] ? this.$slots['inactive-icon']() : this.$slots.default && this.$slots.default()}
+                        </span>
+                        <span class={[css["icon"], css["icon--active"]]}>
+                            {this.$slots['active-icon'] ? this.$slots['active-icon']() : this.$slots.default && this.$slots.default()}
+                        </span>
+                        {/* ${this.renderBadge()} */}
+                    </span>
+    
+                    {(typeof this.label.length !== 'undefined' || this.label !== null) && (
+                        <span class={css['label']}>
+                            {this.label}
+                        </span>
+                    )}
+    
+                </button>)
+        }
+    })
+    
+}
+
+export const NavigationTab = new NavigationTabComponent().component
