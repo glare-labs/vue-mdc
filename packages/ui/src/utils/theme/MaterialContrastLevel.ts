61bdc9b4073c50f8b80cb43d3beec9d85bfafe60
Bre 97 <by.sgatn3@Gmail.com>
2024-06-22 19:01:37 +0800

    fix: remove const flag from enum EMaterialContrastLevel

--- packages/ui/src/utils/theme/MaterialContrastLevel.ts
+++ packages/ui/src/utils/theme/MaterialContrastLevel.ts
@@ -1,6 +1,6 @@
 export type TMaterialContrastLevel = -1.0 | 0 | 0.5 | 1.0
 
-export const enum EMaterialContrastLevel {
+export enum EMaterialContrastLevel {
     Reduced = -1.0,
     Default = 0,
     Medium = 0.5,
