9696e2d52ecf85ee37be51db91f323568de2ad1d
bre97-web <bre97-web@outlook.com>
2024-10-06 12:43:00 +0800

    chore: move switch-on-icon and switch-off-icon to class as fields

--- packages/ui/src/components/switch/switch.tsx
+++ packages/ui/src/components/switch/switch.tsx
@@ -36,6 +36,24 @@ class SwitchComponent {
         'change'
     ]
 
+    private static readonly switchOnIcon = (
+        <div class={[css.icon, css['icon--on']]}>
+            <svg viewBox="0 0 24 24">
+                <path d="M9.55 18.2 3.65 12.3 5.275 10.675 9.55 14.95 18.725 5.775 20.35 7.4Z" />
+            </svg>
+        </div>
+    )
+
+    private static readonly switchOffIcon = (
+        <div class={[css.icon, css['icon--off']]}>
+            <svg viewBox="0 0 24 24">
+                <path
+                    d="M6.4 19.2 4.8 17.6 10.4 12 4.8 6.4 6.4 4.8 12 10.4 17.6 4.8 19.2 6.4 13.6 12 19.2 17.6 17.6 19.2 12 13.6Z"
+                />
+            </svg>
+        </div>
+    )
+
     public readonly component = defineComponent({
         name: this.name,
         props: this.props,
@@ -49,13 +67,13 @@ class SwitchComponent {
             }
         },
         mounted() {
-            if(isServer()) {
+            if (isServer()) {
                 return
             }
             (this.$el as Element).addEventListener('click', this.handleClick)
         },
         beforeUpdate() {
-            if(this.modelValue !== null) {
+            if (this.modelValue !== null) {
                 this.setSelected(this.modelValue)
             }
         },
@@ -67,7 +85,7 @@ class SwitchComponent {
         }),
         methods: {
             setSelected(selected: boolean) {
-                if(this.selected === selected) {
+                if (this.selected === selected) {
                     return
                 }
                 this.selected = selected
@@ -82,6 +100,23 @@ class SwitchComponent {
         },
 
         render() {
+            const SwitchOnIcon = () => SwitchComponent.switchOnIcon
+            const SwitchOffIcon = () => SwitchComponent.switchOffIcon
+            const renderIcon = (
+                <span class={[css.handle, (this.withIconSelectedOnly || this.withIcon) && css['with-icon']]}>
+                    {
+                        (this.withIconSelectedOnly || this.withIcon) &&
+                        <div class={css.icons}>
+                            {
+                                this.selected && (this.$slots['on-icon'] ? this.$slots['on-icon']() : <SwitchOnIcon></SwitchOnIcon>)
+                            }
+                            {
+                                !this.withIconSelectedOnly && !this.selected && (this.$slots['off-icon'] ? this.$slots['off-icon']() : <SwitchOffIcon></SwitchOffIcon>)
+                            }
+                        </div>
+                    }
+                </span>)
+
             return (
                 <span
                     class={[css.switch, this.disabled && css.disabled, this.selected ? css.selected : css.unselected]}
@@ -103,20 +138,7 @@ class SwitchComponent {
                     <span class={css.track}>
                         <span class={css['handle-container']}>
                             <Ripple></Ripple>
-
-                            <span class={[css.handle, (this.withIconSelectedOnly || this.withIcon) && css['with-icon']]}>
-                                {
-                                    (this.withIconSelectedOnly || this.withIcon) &&
-                                    <div class={css.icons}>
-                                        {
-                                            this.selected && (this.$slots['on-icon'] ? this.$slots['on-icon']() : <SwitchOnIcon></SwitchOnIcon>)
-                                        }
-                                        {
-                                            !this.withIconSelectedOnly && !this.selected && (this.$slots['off-icon'] ? this.$slots['off-icon']() : <SwitchOffIcon></SwitchOffIcon>)
-                                        }
-                                    </div>
-                                }
-                            </span>
+                            {renderIcon}
                         </span>
 
 
@@ -128,25 +150,3 @@ class SwitchComponent {
 }
 
 export const Switch = new SwitchComponent().component
-
-function SwitchOnIcon() {
-    return (
-        <div class={[css.icon, css['icon--on']]}>
-            <svg viewBox="0 0 24 24">
-                <path d="M9.55 18.2 3.65 12.3 5.275 10.675 9.55 14.95 18.725 5.775 20.35 7.4Z" />
-            </svg>
-        </div>
-    )
-}
-
-function SwitchOffIcon() {
-    return (
-        <div class={[css.icon, css['icon--off']]}>
-            <svg viewBox="0 0 24 24">
-                <path
-                    d="M6.4 19.2 4.8 17.6 10.4 12 4.8 6.4 6.4 4.8 12 10.4 17.6 4.8 19.2 6.4 13.6 12 19.2 17.6 17.6 19.2 12 13.6Z"
-                />
-            </svg>
-        </div>
-    )
-}
