0dc32b173bafc8d5cf6fe2ee5bb1af20bccd87e4
bre97-web <bre97-web@outlook.com>
2024-10-06 13:48:20 +0800

    feat: add href and target to icon-button

--- packages/ui/src/components/icon-button/icon-button.tsx
+++ packages/ui/src/components/icon-button/icon-button.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, type PropType, type SlotsType } from 'vue'
+import type { TButtonTarget } from '../../utils/button-target-type'
 import { Ripple } from '../ripple/ripple'
 import { EIconButtonAppearance, type TIconButtonAppearance } from './icon-button-appearance'
 import { EIconButtonType, type TIconButtonType } from './icon-button-type'
@@ -19,6 +20,14 @@ class IconButtonComponent {
             type: String as PropType<TIconButtonType>,
             default: EIconButtonType.Button,
         },
+        href: {
+            type: String as PropType<string>,
+            default: null,
+        },
+        target: {
+            type: String as PropType<TButtonTarget | ''>,
+            default: '',
+        }
     }
     private slots = {} as SlotsType<{
         default: void
@@ -29,32 +38,48 @@ class IconButtonComponent {
         props: this.props,
         slots: this.slots,
         render() {
+            const isLink = this.href !== null
+
             const renderIcon = (
                 <span class={css.icon}>
                     {this.$slots.default && this.$slots.default()}
                 </span>
             )
-
-            return (
+            const renderContent = (
+                <>
+                    <Ripple></Ripple>
+                    <div aria-hidden="true" class={css.touch}></div>
+                    <div aria-hidden="true" class={css.background}></div>
+                    <div aria-hidden="true" class={css.outline}></div>
+                    {renderIcon}
+                </>
+            )
+            const renderLink = (
+                <a
+                    class={[css[this.appearance], this.disabled && css.disabled]}
+                    href={this.href}
+                    target={this.target}
+                    data-component="icon-button"
+                    aria-disabled={this.disabled}
+                    type={this.type}
+                >
+                    {renderContent}
+                </a>
+            )
+            const renderButton = (
                 <button
-                    class={[
-                        css[this.appearance],
-                        this.disabled && css.disabled
-                    ]}
+                    class={[css[this.appearance], this.disabled && css.disabled]}
                     data-component="icon-button"
                     disabled={this.disabled}
                     aria-disabled={this.disabled}
                     type={this.type}
                 >
-                    <Ripple></Ripple>
-
-                    <div aria-hidden="true" class={css.touch}></div>
-                    <div aria-hidden="true" class={css.background}></div>
-                    <div aria-hidden="true" class={css.outline}></div>
-
-                    {renderIcon}
+                    {renderContent}
                 </button>
             )
+
+
+            return isLink ? renderLink : renderButton
         }
     })
 }
