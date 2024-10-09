e14e31bdad42d01f5012d30bf217f651dd9a7662
bre97-web <bre97-web@outlook.com>
2024-10-09 12:29:16 +0800

    refactor: remove directive register, create component-register module

---
+++ packages/ui/src/component-register/component-register.ts
@@ -0,0 +1,43 @@
+import type { App } from 'vue'
+import { Button } from '../components/button'
+import { Checkbox } from '../components/checkbox'
+import { Divider } from '../components/divider'
+import { Elevation } from '../components/elevation'
+import { Fab } from '../components/fab'
+import { GlareProvider } from '../components/glare-provider'
+import { Icon } from '../components/icon'
+import { IconButton, ToggleIconButton } from '../components/icon-button'
+import { NavigationBar } from '../components/navigation-bar'
+import { NavigationRail } from '../components/navigation-rail'
+import { NavigationRailTab } from '../components/navigation-rail-tab'
+import { NavigationTab } from '../components/navigation-tab'
+import { CircularProgress, LinearProgress } from '../components/progress'
+import { Radio } from '../components/radio'
+import { Ripple } from '../components/ripple'
+import { Switch } from '../components/switch'
+import { Typography } from '../components/typography'
+import { componentNamePrefix } from '../internal/component-name-prefix/component-name-prefix'
+
+export const registerComponents = (prefix = componentNamePrefix) => ({
+    install: (instance: App<any>) => {
+        instance.component(`${prefix}-button`, Button)
+        instance.component(`${prefix}-checkbox`, Checkbox)
+        instance.component(`${prefix}-divider`, Divider)
+        instance.component(`${prefix}-elevation`, Elevation)
+        instance.component(`${prefix}-fab`, Fab)
+        instance.component(`${prefix}-provider`, GlareProvider)
+        instance.component(`${prefix}-icon`, Icon)
+        instance.component(`${prefix}-icon-button`, IconButton)
+        instance.component(`${prefix}-toggle-icon-button`, ToggleIconButton)
+        instance.component(`${prefix}-navigation-bar`, NavigationBar)
+        instance.component(`${prefix}-navigation-tab`, NavigationTab)
+        instance.component(`${prefix}-navigation-rail`, NavigationRail)
+        instance.component(`${prefix}-navigation-rail-tab`, NavigationRailTab)
+        instance.component(`${prefix}-linear-progress`, LinearProgress)
+        instance.component(`${prefix}-circular-progress`, CircularProgress)
+        instance.component(`${prefix}-radio`, Radio)
+        instance.component(`${prefix}-ripple`, Ripple)
+        instance.component(`${prefix}-switch`, Switch)
+        instance.component(`${prefix}-typography`, Typography)
+    }
+})
