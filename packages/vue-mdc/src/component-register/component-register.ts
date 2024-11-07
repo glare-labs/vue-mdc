8e22fafe616646d5dc1d161f3eb87d8d3b141add
Bre 97 <bre97.web@gmail.com>
2024-11-07 21:27:11 +0800

    chore: update component-register and package.json

--- packages/vue-mdc/src/component-register/component-register.ts
+++ packages/vue-mdc/src/component-register/component-register.ts
@@ -1,9 +1,12 @@
 import type { App } from 'vue'
 import { Button } from '../components/button'
 import { Checkbox } from '../components/checkbox'
+import { Dialog } from '../components/dialog'
 import { Divider } from '../components/divider'
 import { Elevation } from '../components/elevation'
 import { Fab } from '../components/fab'
+import { Field } from '../components/field'
+import { FocusRing } from '../components/focus-ring'
 import { Icon } from '../components/icon'
 import { IconButton, ToggleIconButton } from '../components/icon-button'
 import { NavigationBar } from '../components/navigation-bar'
@@ -11,7 +14,7 @@ import { NavigationRail } from '../components/navigation-rail'
 import { NavigationRailTab } from '../components/navigation-rail-tab'
 import { NavigationTab } from '../components/navigation-tab'
 import { CircularProgress, LinearProgress } from '../components/progress'
-import { Provider } from '../components/provider'
+import { ThemeProvider } from '../components/provider'
 import { Radio } from '../components/radio'
 import { Ripple } from '../components/ripple'
 import { Switch } from '../components/switch'
@@ -22,9 +25,12 @@ export const registerComponents = (prefix = componentNamePrefix) => ({
     install: (instance: App<any>) => {
         instance.component(`${prefix}-button`, Button)
         instance.component(`${prefix}-checkbox`, Checkbox)
+        instance.component(`${prefix}-dialog`, Dialog)
         instance.component(`${prefix}-divider`, Divider)
         instance.component(`${prefix}-elevation`, Elevation)
         instance.component(`${prefix}-fab`, Fab)
+        instance.component(`${prefix}-field`, Field)
+        instance.component(`${prefix}-focus-ring`, FocusRing)
         instance.component(`${prefix}-icon`, Icon)
         instance.component(`${prefix}-icon-button`, IconButton)
         instance.component(`${prefix}-toggle-icon-button`, ToggleIconButton)
@@ -34,7 +40,7 @@ export const registerComponents = (prefix = componentNamePrefix) => ({
         instance.component(`${prefix}-navigation-rail-tab`, NavigationRailTab)
         instance.component(`${prefix}-linear-progress`, LinearProgress)
         instance.component(`${prefix}-circular-progress`, CircularProgress)
-        instance.component(`${prefix}-provider`, Provider)
+        instance.component(`${prefix}-theme-provider`, ThemeProvider)
         instance.component(`${prefix}-radio`, Radio)
         instance.component(`${prefix}-ripple`, Ripple)
         instance.component(`${prefix}-switch`, Switch)
