0552b5a6d45cec65f72910489ad8a4f16da7919f
Bre 97 <bre97.web@gmail.com>
2025-04-27 20:14:02 +0800

    fix: fixed the issue where ripple was still visible when radio was disabled, and the focus-ring could not focus properly

--- packages/vue-mdc/src/components/radio/radio.tsx
+++ packages/vue-mdc/src/components/radio/radio.tsx
@@ -6,10 +6,14 @@ import { FocusRing } from '../focus-ring'
 import { Ripple } from '../ripple/ripple'
 import css from './styles/radio.module.scss'
 
-class RadioComponent {
-    private readonly name = `${componentNamePrefix}-radio`
-
-    private readonly props = {
+export const Radio = defineComponent({
+    name: `${componentNamePrefix}-radio`,
+    slots: {} as SlotsType<{}>,
+    emits: [
+        'update:modelValue',
+        'change'
+    ],
+    props: {
         disabled: {
             type: Boolean as PropType<boolean>,
             default: false,
@@ -22,134 +26,126 @@ class RadioComponent {
             type: Boolean as PropType<boolean>,
             default: null,
         },
-    }
-    private readonly slots = {} as SlotsType<{}>
-    private readonly emits = [
-        'update:modelValue',
-        'change'
-    ]
+    },
+    setup(props) {
 
-    public readonly component = defineComponent({
-        name: this.name,
-        props: this.props,
-        slots: this.slots,
-        emits: this.emits,
-        created() {
-            if (this.modelValue === null) {
-                this.checked = this.defaultChecked
-                this.$emit('update:modelValue', this.defaultChecked)
+        // const _disabled = ref()
+    },
+    created() {
+        if (this.modelValue === null) {
+            this.checked = this.defaultChecked
+            this.$emit('update:modelValue', this.defaultChecked)
 
-            } else {
-                this.checked = this.modelValue
-            }
+        } else {
+            this.checked = this.modelValue
+        }
+    },
+    mounted() {
+        if (isServer()) {
+            return
+        }
+        (this.$el as Element).addEventListener('click', this.handleClick)
+
+        const maskElement = (this.$el as Element).querySelector('&>svg>.mask')
+        const needMaskElement = (this.$el as Element).querySelector('&>svg>.need-mask')
+        if (this.maskId === null) {
+            this.maskId = generateUuid()
+        }
+        maskElement?.setAttribute('id', this.maskId)
+        needMaskElement?.setAttribute('mask', `url(#${this.maskId})`)
+
+    },
+    beforeUpdate() {
+        if (this.modelValue !== null) {
+            this.setChecked(this.modelValue)
+        }
+    },
+    beforeUnmount() {
+        (this.$el as Element).removeEventListener('click', this.handleClick)
+    },
+    data: () => ({
+        checked: false,
+        /**
+         * Unique id for mask.
+         */
+        maskId: null as string | null
+    }),
+    methods: {
+        getRootElement(): HTMLElement | null {
+            return this.$el
         },
-        mounted() {
-            if (isServer()) {
+        getFocusRingElement(): HTMLElement | null {
+            return (this.$el as HTMLElement).querySelector('[data-component="focus-ring"]')
+        },
+        getInputElement(): HTMLElement | null {
+            return (this.$el as HTMLElement).querySelector(`input.${css.input}`)
+        },
+        setChecked(checked: boolean) {
+            const changeEvent = new Event('change', { cancelable: true })
+            this.$emit('change', changeEvent)
+            const preventChange = !dispatchEvent(changeEvent)
+            if (preventChange) {
                 return
             }
-            (this.$el as Element).addEventListener('click', this.handleClick)
-
-            const maskElement = (this.$el as Element).querySelector('&>svg>.mask')
-            const needMaskElement = (this.$el as Element).querySelector('&>svg>.need-mask')
-            if (this.maskId === null) {
-                this.maskId = generateUuid()
+            if (this.checked === checked) {
+                return
             }
-            maskElement?.setAttribute('id', this.maskId)
-            needMaskElement?.setAttribute('mask', `url(#${this.maskId})`)
+            this.checked = checked
+            this.$emit('update:modelValue', checked)
         },
-        beforeUpdate() {
-            if (this.modelValue !== null) {
-                this.setChecked(this.modelValue)
+        handleClick() {
+            if (this.disabled) {
+                return
             }
+            this.setChecked(!this.checked)
         },
-        beforeUnmount() {
-            (this.$el as Element).removeEventListener('click', this.handleClick)
-        },
-        data: () => ({
-            checked: false,
-            /**
-             * Unique id for mask.
-             */
-            maskId: null as string | null
-        }),
-        methods: {
-            setChecked(checked: boolean) {
-                const changeEvent = new Event('change', { cancelable: true })
-                this.$emit('change', changeEvent)
-                const preventChange = !dispatchEvent(changeEvent)
-                if (preventChange) {
-                    return
-                }
-                if (this.checked === checked) {
-                    return
-                }
-                this.checked = checked
-                this.$emit('update:modelValue', checked)
-            },
-            handleClick() {
-                if (this.disabled) {
-                    return
-                }
-                this.setChecked(!this.checked)
-            },
-        },
+    },
 
-        render() {
-            const id = `radio-${generateUuid()}`
-            return (
-                <div
-                    data-component="radio"
-                    role="radio"
-                    class={[css.radio, this.checked && css.checked, this.disabled && css.disabled]}
+    render() {
+        return (
+            <div
+                data-component="radio"
+                role="radio"
+                class={[css.radio, this.checked && css.checked, this.disabled && css.disabled]}
+                tabindex={0}
+            >
+                <Ripple disabled={this.disabled}></Ripple>
+                <FocusRing shapeInherit={false}></FocusRing>
+
+                <svg
+                    aria-hidden="true"
+                    class={css.icon}
+                    viewBox="0 0 20 20"
                 >
-                    <Ripple></Ripple>
-                    <FocusRing htmlFor={id} shapeInherit={false}></FocusRing>
-
-                    <svg
-                        aria-hidden="true"
-                        class={css.icon}
-                        viewBox="0 0 20 20"
-                    >
-                        <mask class="mask">
-                            <rect
-                                width="100%"
-                                height="100%"
-                                fill="white"
-                            />
-                            <circle
-                                cx="10"
-                                cy="10"
-                                r="8"
-                                fill="black"
-                            />
-                        </mask>
-                        <circle
-                            class={["need-mask", css.outer, css.circle]}
-                            cx="10"
-                            cy="10"
-                            r="10"
+                    <mask class="mask">
+                        <rect
+                            width="100%"
+                            height="100%"
+                            fill="white"
                         />
                         <circle
-                            class={[css.inner, css.circle]}
                             cx="10"
                             cy="10"
-                            r="5"
+                            r="8"
+                            fill="black"
                         />
-                    </svg>
-
-                    <input
-                        type="radio"
-                        class={css.input}
-                        checked={this.checked}
-                        disabled={this.disabled}
-                        aria-hidden="true"
-                        id={id}
+                    </mask>
+                    <circle
+                        class={["need-mask", css.outer, css.circle]}
+                        cx="10"
+                        cy="10"
+                        r="10"
                     />
-                </div>
-            )
-        },
-    })
+                    <circle
+                        class={[css.inner, css.circle]}
+                        cx="10"
+                        cy="10"
+                        r="5"
+                    />
+                </svg>
 
-}
-
-export const Radio = new RadioComponent().component
+                <span class={css['touch-target']}></span>
+            </div>
+        )
+    },
+})
