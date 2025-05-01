dbb35fc553406eb9a974d462c9318314348ac08b
Bre 97 <bre97.web@gmail.com>
2025-05-01 21:21:35 +0800

    refactor: apply renderNavigationDestination to NavigationBar

--- packages/vue-mdc/src/components/navigation-bar/navigation-bar.tsx
+++ packages/vue-mdc/src/components/navigation-bar/navigation-bar.tsx
@@ -1,90 +1,89 @@
-import { defineComponent, type PropType, type SlotsType } from 'vue'
+import { useReflectAttribute } from '@glare-labs/vue-reflect-attribute'
+import { defineComponent, ref, type PropType, type SlotsType } from 'vue'
 import { componentNamePrefix } from '../../internals/component-name-prefix/component-name-prefix'
-import { NavigableController, type INavigableElementEventMap, type TNavigableElementChangeEventDetail } from '../../internals/controller/navigable-controller'
-import { isServer } from '../../utils/is-server'
+import { useNavigable, type INavigationItem } from '../../internals/controller/use-navigable'
+import { renderNavigationDestination } from '../../internals/navigation/render-navigation-destination'
 import { Elevation } from '../elevation'
-import type { TNavigationRailTabClickEventDetail } from '../navigation-rail-tab'
-import type { TNavigationTabClickEvent } from '../navigation-tab'
+import { NavigationTab } from '../navigation-tab'
 import css from './styles/navigation-bar.module.scss'
 
-class NavigationBarComponent {
-    private readonly name = `${componentNamePrefix}-navigation-bar`
+export enum ENavigationBarRippleStyle {
+    Default = 'default',
+    Unbounded = 'unbounded',
+    Bounded = 'bounded',
+}
 
-    private readonly props = {
-        defaultActiveIndex: {
+export type TNavigationBarRippleStyle = 'default' | 'unbounded' | 'bounded'
+
+export const NavigationBar = defineComponent({
+    name: `${componentNamePrefix}-navigation-bar`,
+    slots: {} as SlotsType<{
+        default: void
+    }>,
+    props: {
+        defaultActiveOrder: {
             type: Number as PropType<number>,
-            default: 0,
+            default: -2,
         },
-        rippleUnbounded: {
+        rippleStyle: {
+            type: String as PropType<TNavigationBarRippleStyle>,
+            default: ENavigationBarRippleStyle.Default,
+        },
+        tabs: {
+            type: Array as PropType<Array<INavigationItem>>,
+            default: [],
+        },
+        hideInactiveLabel: {
             type: Boolean as PropType<boolean>,
             default: false,
         }
-    }
-    private readonly slots = {} as SlotsType<{
-        default: void
-    }>
-    private readonly emits: Array<keyof INavigableElementEventMap> = [
+    },
+    emits: [
         'change'
-    ]
+    ],
+    setup(props, { slots, emit }) {
+        const root = ref<HTMLElement | null>(null)
 
-    public readonly component = defineComponent({
-        name: this.name,
-        props: this.props,
-        slots: this.slots,
-        emits: this.emits,
-        mounted() {
-            if (isServer()) {
-                return
-            }
-            this.navigableController = new NavigableController(this.$el)
-            this.navigableController.activeIndex = this.defaultActiveIndex
-            this.navigableController.host.addEventListener('tab-click', this.handleTabClick)
-        },
-        beforeUnmount() {
-            this.navigableController?.host.removeEventListener('tab-click', this.handleTabClick)
-        },
-        data: () => ({
-            navigableController: null as null | NavigableController,
-        }),
-        methods: {
-            handleTabClick(e: Event) {
-                e.preventDefault()
-                e.stopPropagation()
-                if (this.navigableController === null) {
-                    return
-                }
-                const eventTab = (e as TNavigationTabClickEvent).detail.tab
-                const changeEvent = new CustomEvent<TNavigableElementChangeEventDetail>('change', {
-                    cancelable: true,
-                    bubbles: true,
-                    composed: true,
-                    detail: {
-                        indexAfterUpdate: this.navigableController.getNavigationTabs().findIndex(tab => tab === eventTab),
-                        indexBeforeUpdate: this.navigableController.activeIndex,
-                        label: (e as TNavigationTabClickEvent).detail.label
-                    }
+        const _rippleStyle = ref(props.rippleStyle)
+
+        const handleChange = (e: Event) => {
+            emit('change', e)
+        }
+
+        useReflectAttribute(root, {
+            attributes: [
+                { attribute: 'ripple-style', ref: _rippleStyle, reflect: true, type: 'string' },
+            ],
+            tick: 'after'
+        })
+
+        useNavigable(root, {
+            tabs: [],
+            handleChange,
+        })
+
+        return () => {
+            renderNavigationDestination({
+                component: NavigationTab,
+                container: root,
+                tabs: props.tabs,
+                props: (tabConfig) => ({
+                    hideInactiveLabel: typeof tabConfig.hideInactiveLabel === 'undefined' ? props.hideInactiveLabel : tabConfig.hideInactiveLabel,
+                    label: tabConfig.label ?? 'Unnamed Tab',
                 })
-                this.$emit('change', changeEvent)
-                const preventChange = !this.navigableController.host.dispatchEvent(changeEvent)
-                if (preventChange) {
-                    return
-                }
-                this.navigableController.activeIndex = this.navigableController.getNavigationTabs().findIndex(tab => tab === (e as CustomEvent<TNavigationRailTabClickEventDetail>).detail.tab)
-            }
-        },
-        render() {
+            })
+
             return (
                 <div
                     data-component="navigation-bar"
-                    class={[css['navigation-bar'], this.rippleUnbounded && css['ripple-unbounded']]}
+                    class={[css['navigation-bar'], _rippleStyle.value === ENavigationBarRippleStyle.Unbounded && css['ripple-unbounded']]}
+                    ref={root}
                 >
-                    {this.$slots.default && this.$slots.default()}
+                    {slots.default && slots.default()}
 
                     <Elevation></Elevation>
                 </div>
             )
         }
-    })
-}
-
-export const NavigationBar = new NavigationBarComponent().component
+    },
+})
