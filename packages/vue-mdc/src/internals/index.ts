e3e887c0cbcd6627d58c1ea352039f0118ea8397
Bre 97 <bre97.web@gmail.com>
2025-05-04 12:14:13 +0800

    feat: dispatch-hooks

--- packages/vue-mdc/src/internals/index.ts
+++ packages/vue-mdc/src/internals/index.ts
@@ -1,6 +1,7 @@
 export * from './controller/form-submitter'
 export * from './controller/use-attachable'
 export * from './controller/use-navigable'
+export * from './events/dispatch-hooks'
 export * from './events/form-label-activation'
 export * from './events/redispatch-event'
 export * from './navigation/render-navigation-destination'
