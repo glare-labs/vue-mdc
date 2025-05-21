18e2a8a136026038aa8000ef78d660bc3e851b5e
bre97-web <bre97.web@gmail.com>
2025-05-21 20:40:11 +0800

    chore: create index.ts to export all components and other features, update tsconfig

---
+++ packages/vue-mdc/src/index.ts
@@ -0,0 +1,47 @@
+
+/**
+ * colors
+ */
+export * from './colors'
+
+/**
+ * component-register
+ */
+export * from './component-register'
+
+/**
+ * components
+ */
+export * from './components/bottom-app-bar'
+export * from './components/button'
+export * from './components/checkbox'
+export * from './components/dialog'
+export * from './components/divider'
+export * from './components/elevation'
+export * from './components/fab'
+export * from './components/field'
+export * from './components/focus-ring'
+export * from './components/icon'
+export * from './components/icon-button'
+export * from './components/navigation-bar'
+export * from './components/navigation-drawer'
+export * from './components/navigation-rail'
+export * from './components/navigation-rail-tab'
+export * from './components/navigation-tab'
+export * from './components/progress'
+export * from './components/radio'
+export * from './components/ripple'
+export * from './components/split-button'
+export * from './components/switch'
+export * from './components/tooltip'
+export * from './components/typography'
+
+/**
+ * internals
+ */
+export * from './internals'
+
+/**
+ * utils
+ */
+export * from './utils'
